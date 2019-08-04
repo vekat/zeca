2c56c65896bbeb8f89877977b1e557f461e7f35b
Vitor Cortez <vitor.a.cortez@gmail.com>
2019-08-04 19:27:50 -0300

    Update zoeira

--- cogs/zoeira.py
+++ cogs/zoeira.py
@@ -12,34 +12,36 @@ class Zoeira:
     @commands.command(name='ban', aliases=[])
     async def _ban(self, ctx, *, member: discord.Member):
         """For the craic."""
-        
+
         author = ctx.author.display_name
-        
+
         if member in ctx.message.mentions:
             target = member.display_name
         else:
             target = member.mention
-        
+
         if author == member.display_name:
             message = ':hammer: | **' + author + \
-            ' has hammered their thumb. What a shame.**'
-        
+                ' has hammered their thumb. What a shame.**'
+        if ctx.author.id == 119819028414857217:
+            message = '<:marreta:607695189410054332> | **' + target + \
+                ' levou uma marretada! ' + author + ' suspeitou desde o princípio**.'
         elif author == 'fausthanos':
             message = ':hammer: | **' + target + \
                 ' has been slain by Fausthanos, for the good of the Universe.**'
-                
+
         elif author == 'charon':
             message = ':hammer: | ** Charon has made a dinner reservation for ' + \
                 target + '**'
-            
+
         elif author == 'winston':
             message = ':hammer: | ** Winston has acted upon ' + \
                 target + '\'s own actions.**'
-        
+
         else:
             message = ':hammer: | **' + author + \
-            ' has banned ' + target + '**.'
-        
+                ' has banned ' + target + '**.'
+
         await ctx.send(message)
 
     @commands.command(name='beijunda', aliases=['bd'])
@@ -55,7 +57,7 @@ class Zoeira:
             ' mandou um beijunda para ' + target + '**.'
 
         await ctx.send(message)
-    
+
     @commands.command(name='shutupvekat')
     @commands.guild_only()
     @commands.cooldown(1, 600, commands.BucketType.guild)
@@ -63,14 +65,16 @@ class Zoeira:
         """Manda o vekat catar coquinhos."""
 
         vekat = ctx.guild.get_member(119819028414857217)
-        
+
         if vekat is None:
             print('404 vekat not found')
             return
 
         embed = discord.Embed(title='Friendly reminder')
-        embed.set_image(url="https://cdn.discordapp.com/attachments/256903500536086538/413688750007123969/Screenshot_8.png")
-        embed.set_footer(text="If vekat doesn't stop after this, you may ban him.")
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/256903500536086538/413688750007123969/Screenshot_8.png")
+        embed.set_footer(
+            text="If vekat doesn't stop after this, you may ban him.")
 
         await ctx.send(content='Hey ' + vekat.mention, embed=embed)
 
