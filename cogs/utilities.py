9395a6195dd27828fe150dbcd5a2b19fb083941a
Vitor Cortez <vitor.a.cortez@gmail.com>
2019-11-21 14:14:10 -0300

    Fix indentation

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -60,16 +60,16 @@ class Utilities(commands.Cog):
   @commands.command(name='role', aliases=['r'])
   async def _role(self, ctx, *, role):
     """
-        Assigns or removes a role.
+    Assigns or removes a role.
 
-        If role is a level role (Level A, Level B, Level C, or Native),
-        the previous level will be automatically removed.
+    If role is a level role (Level A, Level B, Level C, or Native),
+    the previous level will be automatically removed.
 
-        The role 'hit me up' expires after 1 hour.
+    The role 'hit me up' expires after 1 hour.
 
-        Sub-command:
-            list:  Shows a list of public roles.
-        """
+    Sub-command:
+        list:  Shows a list of public roles.
+    """
 
     if role.lower() in list(self.public_roles.keys()):
       role = self.public_roles[role.lower()]
@@ -111,10 +111,10 @@ class Utilities(commands.Cog):
   @commands.command(name='dicinf', aliases=['di', 'dicinformal'])
   async def _dicionarioinformal(self, ctx, *term):
     """
-        Looks a word up in the Dicionário Informal.
+    Looks a word up in the Dicionário Informal.
 
-        Don't trust this dictionary blindly. It's like a Brazilian Urban Dictionary.
-        """
+    Don't trust this dictionary blindly. It's like a Brazilian Urban Dictionary.
+    """
     def _meaning(entry):
       result = dicinformal.Query(entry)
       embed = discord.Embed(
@@ -154,62 +154,63 @@ class Utilities(commands.Cog):
 
   @commands.command(name='priberam', aliases=['pri'])
   async def _priberam(self, ctx, *, entry):
-    """ Looks up a word in the Priberam Portuguese dictionary.
+    """
+    Looks up a word in the Priberam Portuguese dictionary.
 
-        The definitions and examples might be in pre-1990 Agreement
-        Portuguese. Make sure to check the footer for possible changes.
+    The definitions and examples might be in pre-1990 Agreement
+    Portuguese. Make sure to check the footer for possible changes.
 
-        https://www.priberam.pt/
-        https://en.wikipedia.org/wiki/Portuguese_Language_Orthographic_Agreement_of_1990
-        """
-        results = priberamdict.Entry(entry)
-        d = results.definitions
-        s = results.suggestions
+    https://www.priberam.pt/
+    https://en.wikipedia.org/wiki/Portuguese_Language_Orthographic_Agreement_of_1990
+    """
+    results = priberamdict.Entry(entry)
+    d = results.definitions
+    s = results.suggestions
 
-        if d:
-            output = d[0]
-            t = results.table_of_contents[0]
-            if t['affect']:
-                output = output + '_Após o acordo ortográfico:_ **' + \
-                    t['br_aft'] + '** 🇧🇷, **' + t['pt_aft'] + '** 🇵🇹.\n'
-            else:
-                output = output + '_Grafias:_ **' + \
-                    t['br_bef'] + '** 🇧🇷, **' + t['pt_bef'] + '** 🇵🇹.\n'
+    if d:
+      output = d[0]
+      t = results.table_of_contents[0]
+      if t['affect']:
+        output = output + '_Após o acordo ortográfico:_ **' + \
+            t['br_aft'] + '** 🇧🇷, **' + t['pt_aft'] + '** 🇵🇹.\n'
+      else:
+        output = output + '_Grafias:_ **' + \
+            t['br_bef'] + '** 🇧🇷, **' + t['pt_bef'] + '** 🇵🇹.\n'
 
-        if isinstance(s, list):
-            output = 'Palavra não encontrada. '
-            if s:
-                output = output + 'Aqui estão algumas sugestões:\n'
-                output = output + ' '.join(s)
+    if isinstance(s, list):
+      output = 'Palavra não encontrada. '
+      if s:
+        output = output + 'Aqui estão algumas sugestões:\n'
+        output = output + ' '.join(s)
 
-        await ctx.send(output)
+    await ctx.send(output)
 
-    @_priberam.error
-    async def __priberam_error(self, ctx, error):
-        await ctx.send(error.__cause__)
+  @_priberam.error
+  async def __priberam_error(self, ctx, error):
+    await ctx.send(error.__cause__)
 
-    @commands.command()
-    async def nick(self, ctx, *, new_nick):
-        """ Changes the nickname of a member. """
-        member = ctx.author
-        await member.edit(nick=new_nick)
+  @commands.command()
+  async def nick(self, ctx, *, new_nick):
+    """ Changes the nickname of a member. """
+    member = ctx.author
+    await member.edit(nick=new_nick)
 
-    @nick.error
-    async def nick_error(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            # await ctx.send(error.__cause__)
-            await ctx.send('I don\'t have permission for that, master.')
+  @nick.error
+  async def nick_error(self, ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+      # await ctx.send(error.__cause__)
+      await ctx.send('I don\'t have permission for that, master.')
 
-    @commands.command(name='correct', aliases=['c'])
-    async def _correct_message(self, ctx, message_id, *, correction):
-        """
-        Highlights the mistakes in a members message provided the message ID followed by the correction.
+  @commands.command(name='correct', aliases=['c'])
+  async def _correct_message(self, ctx, message_id, *, correction):
+    """
+    Highlights the mistakes in a members message provided the message ID followed by the correction.
 
-        - The author of the message you want to correct must have the "Correct me" role.
-        - To get the ID of a message you have to right click the message and then click "Copy ID".
-        - To see the "Copy ID" option you must activate the developer mode.
-            Go to your Settings > Appearance > Advanced > Developer Mode
-        """
+    - The author of the message you want to correct must have the "Correct me" role.
+    - To get the ID of a message you have to right click the message and then click "Copy ID".
+    - To see the "Copy ID" option you must activate the developer mode.
+        Go to your Settings > Appearance > Advanced > Developer Mode
+    """
     target_msg = await ctx.channel.get_message(message_id)
     target_user = target_msg.author
 
@@ -232,8 +233,8 @@ class Utilities(commands.Cog):
   @commands.command(name='urbandictionary', aliases=['urban', 'ud'])
   async def _urbandictionary(self, ctx, *, entry):
     """
-        Look up a word in the Urban Dictionary.
-        """
+    Look up a word in the Urban Dictionary.
+    """
     query = UDQuery(entry)
     definition = query.definition
     definition = re.sub(r'[\[\]]', '', definition)  # Remove [ and ] chars
