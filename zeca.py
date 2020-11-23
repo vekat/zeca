8323bc9a003adf55fa4e8cd22276967c23e0ff52
Vitor Cortez <vitor.a.cortez@gmail.com>
2020-11-23 14:04:28 -0300

    Refactor bot initialization

--- zeca.py
+++ zeca.py
@@ -9,79 +9,79 @@ import asyncio
 
 import private
 
+from pathlib import Path
 from datetime import datetime
 from discord.ext import commands
 
 ROOT = os.path.dirname(sys.modules['__main__'].__file__)
-name_filter = re.compile(r'discord\.gg/\S+', re.I)
 
 intents = discord.Intents.default()
 intents.members = True
+
 
 class Zeca(commands.Bot):
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
 
-    self.blacklist = []
-    self.add_check(self.check_user)
+    self.name_filter = re.compile(r'discord\.gg/\S+', re.I)
+
+    self.main_guild = private.guild
+    self.dialect_channel = private.dialect
+    self.proficiency_channel = private.proficiency
+    self.welcome_hook = private.welcome
+
+    self.setup_blacklist()
+    self.add_check(self.is_main_guild)
+
+  def setup_blacklist(self):
+    self.blacklist_path = private.blacklist_path
+    self.load_blacklist()
+    self.add_check(self.is_not_blacklisted)
+
+  def load_blacklist(self):
+    blp = Path(self.blacklist_path)
+    if not blp.exists():
+      blp.touch()
+
+    with blp.open() as blf:
+      self.blacklisted_ids = [int(l.strip()) for l in blf.readlines()]
+
+  def is_main_guild(self, ctx):
+    return ctx.guild and ctx.guild.id == self.main_guild.id
+
+  def is_not_blacklisted(self, ctx):
+    return ctx.author and ctx.author.id not in self.blacklisted_ids
 
   async def on_ready(self):
-    print('• logged on as:', self.user)
-    print('• discord.py version:', discord.__version__)
+    print(f'→ login successful as: {self.user}')
+    print(f'→ discord.py version: {discord.__version__}')
 
-    await self.load_blacklist()
+    if type(self.welcome_hook) is int:
+      self.welcome_hook = await self.fetch_webhook(self.welcome_hook)
 
-  async def load_blacklist(self):
-    # load blacklisted users
-    try:
-      with open(os.path.join(ROOT, 'blacklist.txt')) as f:
-        self.blacklist = [int(i.strip()) for i in f.readlines()]
-        print('blacklisted IDs:')
-        print(self.blacklist)
-    except FileNotFoundError:
-      with open(os.path.join(ROOT, 'blacklist.txt'), 'w') as f:
-        self.blacklist = []
-        pass
+    if type(self.main_guild) is int:
+      self.main_guild = self.get_guild(self.main_guild)
 
-  def check_user(self, ctx):
-    return ctx.author.id not in self.blacklist
+    if type(self.dialect_channel) is int:
+      self.dialect_channel = self.get_channel(self.dialect_channel)
+
+    if type(self.proficiency_channel) is int:
+      self.proficiency_channel = self.get_channel(self.proficiency_channel)
 
   async def on_member_join(self, member):
-    mid = member.id
-    guild = member.guild
-
+    member_id = member.id
     member_name = str(member)
-    if name_filter.findall(member_name):
-      return await member.ban(reason="[zeca] banned for blacklisted name")
+    if self.name_filter.findall(member_name):
+      return await member.ban(reason=f'[{self.user}] blacklisted name')
 
-    welcome_channel = guild.get_channel(private.welcome)
-    rules_channel = guild.get_channel(private.rules)
-    roles_channel = guild.get_channel(private.roles)
+    await asyncio.sleep(private.welcome_delay)
 
-    await asyncio.sleep(private.delay)
-
-    if not guild.get_member(mid):
+    if not self.main_guild.get_member(member_id):
       return
 
-    embed = discord.Embed(
-        title="<:pt:589471198107402240> Getting started",
-        colour=discord.Colour(0x9b3a),
-        description="Please, read our " + rules_channel.mention,
-        timestamp=datetime.utcfromtimestamp(time.time())
+    return await self.welcome_hook.send(
+        content=private.welcome_message.format(client=self, member=member)
     )
-
-    embed.add_field(
-        name="Get your roles",
-        value=
-        "• First get a proficiency role in <#607329738012491793>\n• Then an optional dialect role in <#607330935133700146>\n\nOr type *role commands* here, as explained in "
-        + roles_channel.mention
-    )
-
-    if welcome_channel:
-      await welcome_channel.send(
-          content="Oi " + member.mention + ", welcome to **Portuguese**",
-          embed=embed
-      )
 
 
 def exit_gracefully(signum, frame):
@@ -105,13 +105,13 @@ if __name__ == '__main__':
       )
   )
 
-  extensions = ['cogs.moderator', 'cogs.utilities', 'cogs.zoeira']
+  exts = ['cogs.moderator', 'cogs.utilities', 'cogs.zoeira']
 
-  for e in extensions:
+  for ext in exts:
     try:
-      bot.load_extension(e)
+      bot.load_extension(ext)
     except Exception as err:
-      print('• failed to load {}'.format(e))
+      print(f'→ error loading extension {ext}')
       print(err)
 
   bot.run(private.TOKEN)
